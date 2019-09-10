9f92e2b2fc13f102723994520d282a905af154c3
Jacob Smith <jacobisaacsmith@outlook.com>
2019-09-10 06:58:20 +0100

    feat(about): update the about command

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,25 +1,22 @@
-import * as sys from 'systeminformation';
 import * as ms from 'ms';
-import * as prettyBytes from 'pretty-bytes';
 
 const about: Command = {
 	run: async (tvf, msg) => {
 		try {
-			const cpu = await sys.cpu();
-			const ram = await sys.mem();
-			const os = await sys.osInfo();
 			const developer = msg.guild.roles.get(tvf.roles.TECHADMIN).members.first().user;
 
 			const embed = tvf
 				.createEmbed('green', false)
 				.setAuthor(developer.tag, developer.avatarURL())
-				.addField('OS', `${os.distro} (${os.release})`)
-				.addField(
-					'CPU',
-					`${cpu.manufacturer} ${cpu.brand} @ ${cpu.speed}GHz`
-				)
-				.addField('Total RAM', prettyBytes(ram.total), true)
+				.setThumbnail(msg.guild.iconURL())
 				.addField('Uptime', ms(tvf.bot.uptime, { long: true }), true)
+				.addBlankField(false)
+				.addField('Members', msg.guild.members.size, true)
+				.addField('Users', msg.guild.members.filter(m => !m.user.bot).size, true)
+				.addField('Bots', msg.guild.members.filter(m => m.user.bot).size, true)
+				.addField('Channels', msg.guild.channels.size, true)
+				.addField('Text Channels', msg.guild.channels.filter(c => c.type === 'text').size, true)
+				.addField('Voice Channels', msg.guild.channels.filter(c => c.type === 'voice').size, true)
 				.setFooter(
 					'Made with ❤ and discord.js',
 					'https://miro.medium.com/max/1200/1*mn6bOs7s6Qbao15PMNRyOA.png'
