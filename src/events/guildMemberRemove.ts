b9a9ee1867ded3ae52e0df2b75600bda4836caa0
newt <itsnewtt@outlook.com>
2020-03-08 00:43:09 +0000

    Update guildMemberRemove.ts

--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -2,7 +2,8 @@ import * as Discord from 'discord.js';
 import Client from '../structures/TVFClient';
 import User from '../models/user';
 
-const guildMemberRemove = (tvf: Client, member: Discord.GuildMember) => tvf.isProduction ? async () => {
+const guildMemberRemove = async (tvf: Client, member: Discord.GuildMember) => {
+        if (tvf.isProduction) {
 	// delete user from database
 	User.findOneAndDelete({ id: member.user.id }).then(() =>
 		tvf.logger.info(`Removed ${member.user.tag} from the database.`),
@@ -11,6 +12,7 @@ const guildMemberRemove = (tvf: Client, member: Discord.GuildMember) => tvf.isPr
 	// send goodbye message
 	const ban = await member.guild.fetchBan(member.user);
 	return tvf.sendToChannel(tvf.channels.GENERAL, ban ? `**${member.user.tag}** has been banned from the Forest.` : `**${member.user.tag}** has exited the Forest.`);
-} : null;
+    }
+};
 
 export default guildMemberRemove;
