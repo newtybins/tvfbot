ede39d3953d2b27d3fd0b1f4f3f7f1630fadc2f6
Newt <itsnewtt@outlook.com>
2020-04-16 18:57:24 +0100

    Rewrite about command.

---
+++ src/commands/about.ts
@@ -0,0 +1,58 @@
+import ms from 'ms';
+
+export default {
+  name: 'about',
+  description: 'Information about the bot and the server!',
+  module: 'Core',
+  aliases: ['stats', 'serverinfo', 'botinfo'],
+  usage: 'about',
+  examples: ['about'],
+  run: async (tvf, msg) => {
+    const developer = msg.guild.roles.cache.get(tvf.roles.techAdmin).members.first().user;
+    const members = await msg.guild.members.fetch();
+    const channels = msg.guild.channels.cache;
+
+    const embed = tvf.createEmbed()
+      .setAuthor(developer.tag, developer.avatarURL())
+      .addFields([
+        {
+          name: 'Uptime',
+          value: ms(tvf.bot.uptime, { long: true }),
+          inline: true,
+        },
+        {
+          name: 'Members',
+          value: members.size,
+          inline: true,
+        },
+        {
+          name: 'Users',
+          value: members.filter(m => !m.user.bot).size,
+          inline: true,
+        },
+        {
+          name: 'Bots',
+          value: members.filter(m => m.user.bot).size,
+          inline: true,
+        },
+        {
+          name: 'Channels',
+          value: channels.filter(c => c.type !== 'category').size,
+          inline: true,
+        },
+        {
+          name: 'Text',
+          value: channels.filter(c => c.type === 'text').size,
+          inline: true,
+        },
+        {
+          name: 'Voice',
+          value: channels.filter(c => c.type === 'voice').size,
+          inline: true,
+        }
+      ])
+      .setFooter(`Made with ❤ and Discord.js by ${developer.tag}`, tvf.bot.user.avatarURL());
+
+      return msg.channel.send(embed);
+  }
+} as Command;
