ee710beeb2aedfe42e189e024e3d8ca85f67c67c
Newt <itsnewtt@outlook.com>
2020-04-16 19:44:14 +0100

    Rewrite compliment command

---
+++ src/commands/compliment.ts
@@ -0,0 +1,24 @@
+export default {
+  name: 'compliment',
+  description: 'Compliment another user in the server!',
+  module: 'Fun',
+  args: true,
+  usage: 'compliment <@user>',
+  examples: ['compliment @newt#1234'],
+  run: async (tvf, msg, args) => {
+    // find the mentioned member
+    const member = tvf.checkForMember(msg, args).user;
+
+    // make the embed
+    const compliment = tvf.compliments[Math.floor(Math.random() * tvf.compliments.length)];
+    const embed = tvf.createEmbed({ thumbnail: false })
+      .setTitle(`${member.username}, ${compliment}`)
+      .setThumbnail(member.avatarURL())
+      .setFooter(`Requested by ${msg.author.username}`, msg.author.avatarURL());
+
+    msg.channel.send(embed);
+
+    // if the mentioned user was the bot
+    if (member === tvf.bot.user) msg.reply('aww, thank you <3');
+  }
+} as Command;
