d2c3c027927cabc77000eb92ae782d7904d34a24
newt <newtykins@outlook.com>
2020-07-13 14:55:22 +0100

    silly mistake fixed

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -51,6 +51,7 @@ export default async (tvf: Client, member: Discord.GuildMember) => {
     // create a document in the database if they don't already have one
     const doc = await User.findOne({ id: member.id }, (err, res) => err ? tvf.logger.error(err) : res) || undefined;
     if (!doc) User.create({ id: member.id });
+    else { doc.inServer = true; tvf.saveDoc(doc); }
 
     // send a DM welcoming the user
     const dmEmbed = tvf.createEmbed()
